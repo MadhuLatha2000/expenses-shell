3acf1412f21f28fcae28fbd527dada46cc333887
MadhuLatha2000 <ammireddylatha@gmail.com>
2024-06-29 22:35:30 +0530

    subitha miss you

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#key=value # after = no space
+Person1=Sunitha
+person2=Madhu
+
+echo "$Person1: ela unnavu?"
+echo "$person2: Bavunna ,nuvvu ela unnavu??"
+echo "$Person1 bavunna $person2,em doing?"
+echo "$person2: working"
+echo "$Person1: ok"
+echo "$person2: hmm!!"
